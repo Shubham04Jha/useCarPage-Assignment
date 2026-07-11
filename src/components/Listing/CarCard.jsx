117599c3c76d80c12c6048341e6e690f81fa3b9b
Shubham04Jha <shubhamjha70491@gmail.com>
2026-07-11 10:32:07 +0530

    ui-fix: additional fuel

--- src/components/Listing/CarCard.jsx
+++ src/components/Listing/CarCard.jsx
@@ -2,8 +2,10 @@ import ImageCarousel from '../ui/ImageCarousel';
 import { useModalContext } from '../../hooks/useModalContext';
 
 function CarCard({car}) {
-  const { stockImages: images, carName, km, fuel, cityName, price, areaName, makeYear } = car;
+  const { stockImages: images, carName, km, fuel, cityName, price, areaName, makeYear, additionalFuel } = car;
   const { openModal } = useModalContext();
+
+  const fuelDisplay = additionalFuel ? `${fuel} + 1` : fuel;
 
   return (
     <article className="car-grid-cell">
@@ -11,7 +13,7 @@ function CarCard({car}) {
 
       <div className="car-grid-cell__content">
         <h3 className="car-grid-cell__title">{makeYear} {carName}</h3>
-        <p className="car-grid-cell__meta">{`${km} km | ${fuel} | ${areaName}, ${cityName}`}</p>
+        <p className="car-grid-cell__meta">{`${km} km | ${fuelDisplay} | ${areaName}, ${cityName}`}</p>
         <p className="car-grid-cell__price">{price}</p>
         <button type="button" className="car-grid-cell__cta" onClick={openModal}>
           Get Seller Details

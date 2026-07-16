fe90e51db658f7c9d72272daa0bfa08580b34c19
Shubham04Jha <shubhamjha70491@gmail.com>
2026-07-16 07:35:21 +0530

    fix error message in makesApi

--- src/api/makesApi.js
+++ src/api/makesApi.js
@@ -3,7 +3,7 @@ import { apiEndpoints } from "./apiConfig";
 export async function getMakes() {
   const response = await fetch(apiEndpoints.makes);
   if (!response.ok) {
-    throw new Error("Failed to fetch cities");
+    throw new Error("Failed to fetch makes");
   }
   return response.json();
 }

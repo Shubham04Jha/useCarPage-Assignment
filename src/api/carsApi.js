71779309cea75a0e727db6e1d6067f324dabeb41
cw-shubhamj <shubham.jha@carwale.com>
2026-07-22 20:43:32 +0530

    test compatible getCars api caller

--- src/api/carsApi.js
+++ src/api/carsApi.js
@@ -4,7 +4,7 @@ import { apiEndpoints } from "./apiConfig";
 export async function getCars(filters, nextPageUrl = null, sort = null, options = {}) {
   const queryStr = buildCarsQuery(filters, sort);
   const url = nextPageUrl || (apiEndpoints.stocks + (queryStr.length ? '?' + queryStr : ''));
-  const response = await fetch(url, options);
+  const response = Object.keys(options).length ? await fetch(url, options) : await fetch(url);
   if (!response.ok) {
     throw new Error("Failed to fetch cars");
   }
